7f988b23c7366114cdfd05c0778e866626ef6aa6
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-02-10 04:42:40 +0400

    MainActivity + binding

--- app/src/main/java/com/yrun/animequote/MainActivity.kt
+++ app/src/main/java/com/yrun/animequote/MainActivity.kt
@@ -2,10 +2,34 @@ package com.yrun.animequote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yrun.animequote.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        uiCallBack = object : UpdateUi {
+            override fun updateUi(uiState: UiState) {
+                uiState.update(binding)
+                viewModel.notifyObserved()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.getButton.setOnClickListener(viewmodel.getQuote())
+        viewModel.StartGettingUpdates(uiCallBack)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.stopGettingUpdates()
     }
 }
