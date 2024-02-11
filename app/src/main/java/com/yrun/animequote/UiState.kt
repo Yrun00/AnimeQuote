039f05546bd175022ced6fde3edd65a2c60bef0a
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-02-11 09:26:28 +0400

    UiState

---
+++ app/src/main/java/com/yrun/animequote/UiState.kt
@@ -0,0 +1,55 @@
+package com.yrun.animequote
+
+import com.yrun.animequote.databinding.ActivityMainBinding
+import android.graphics.Color
+import android.view.View
+
+interface UiState {
+    fun update(binding: ActivityMainBinding)
+
+    data class Error(private val message: String) : UiState {
+        override fun update(binding: ActivityMainBinding) {
+            with(binding) {
+                textView.text = message
+                progressBar.visibility = View.GONE
+                textView.setTextColor(Color.parseColor("#FF0000"))
+                getButton.isEnabled = true
+            }
+        }
+    }
+
+    object Progress : UiState {
+        override fun update(binding: ActivityMainBinding) {
+            with(binding) {
+                progressBar.visibility = View.VISIBLE
+                textView.text = ""
+                getButton.isEnabled = false
+            }
+        }
+    }
+
+    data class Success(
+        private val quote: String = "",
+        private val anime: String = "",
+        private val character: String = ""
+    ) :
+        UiState {
+        override fun update(binding: ActivityMainBinding) {
+            with(binding) {
+                animeTextView.text = "Anime: $anime"
+                characterTextView.text = "Character: $character"
+                textView.text = quote
+                progressBar.visibility = View.GONE
+                textView.setTextColor(Color.parseColor("#000000"))
+                getButton.isEnabled = true
+            }
+        }
+    }
+
+
+    object Empty : UiState {
+        override fun update(binding: ActivityMainBinding) = Unit
+    }
+}
+
+
