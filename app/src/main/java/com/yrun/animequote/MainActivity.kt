ccff547f40ac6a703e9e60fbfdd65dc70b2823e2
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-02-11 09:23:50 +0400

    MainActivity

--- app/src/main/java/com/yrun/animequote/MainActivity.kt
+++ app/src/main/java/com/yrun/animequote/MainActivity.kt
@@ -2,34 +2,43 @@ package com.yrun.animequote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
 import com.yrun.animequote.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ProvideViewModel {
 
     private lateinit var binding:ActivityMainBinding
+    private lateinit var uiCallBack: UpdateUi
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        mainViewModel = viewModel(MainViewModel::class.java)
+        binding.getButton.setOnClickListener{
+            mainViewModel.getQuote()}
 
         uiCallBack = object : UpdateUi {
             override fun updateUi(uiState: UiState) {
                 uiState.update(binding)
-                viewModel.notifyObserved()
             }
         }
     }
 
     override fun onResume() {
         super.onResume()
-        binding.getButton.setOnClickListener(viewmodel.getQuote())
-        viewModel.StartGettingUpdates(uiCallBack)
+        mainViewModel.startGettingUpdates(uiCallBack)
     }
 
     override fun onPause() {
         super.onPause()
-        viewModel.stopGettingUpdates()
+        mainViewModel.stopGettingUpdates()
     }
+
+    override fun <T : ViewModel> viewModel(clazz: Class<out T>): T =
+        (application as ProvideViewModel).viewModel(clazz)
 }
+
